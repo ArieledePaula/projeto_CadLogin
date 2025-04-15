8ac0945156b2ddf6367c89065798f6962ec7abf9
Ariele <arielecohendepaula@gmail.com>
2025-04-15 20:00:04 -0300

    Adicionando Pagina Cadastro e conectando main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+// Implementa acesso para a tela de login
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+
+// Implementar estrutura que testa se todos os campos foram preenchidos
+    if(!loginEmail || !loginSenha){
+        alert("Favor preeencher todos os campos");
+    }else{
+       window.location.href = "cadastro.html";
+    }
+}
