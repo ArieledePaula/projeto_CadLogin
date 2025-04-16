6c90aa60e2c752715f2165867ac7a8e88884c7e2
Ariele <arielecohendepaula@gmail.com>
2025-04-15 21:43:07 -0300

    Implementando função excluir

--- main.js
+++ main.js
@@ -31,7 +31,14 @@ function salvarUser(){
 function criaLista(){
     let tabela = "<tr><td>Nome</td><td>Ações</td></tr>";
     for(let i =0;i<=(dadosLista.length-1); i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-dark'>Editar</button><button class='btn btn-danger'>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-dark' onclick= ''>Editar</button><button class='btn btn-danger' onclick= 'excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
+}
+
+//Função para excluir nome da lista
+function excluir(i){
+    dadosLista.splice((i-1),1);
+    document.getElementById('tabela').deleteRow(i);
+    console.log(i);
 }
